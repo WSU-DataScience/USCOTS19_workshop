e2cf251e3682f8d79557adffa26ba61e59ca68e2
Todd Iverson <Tiverson@winona.edu>
2019-05-12 18:54:03 -0500

    Added rpart and rpart.plot

--- binder/install.R
+++ binder/install.R
@@ -4,6 +4,8 @@ install.packages("reticulate") # python support in RMarkdown
 install.packages("ggplot2") # for plotting
 install.packages("dplyr") # for plotting
 install.packages("tidyr") # for plotting
+install.packages("rpart") # for plotting
+install.packages("rpart.plot") # for plotting
 install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 
 ### install bioconductor packages
